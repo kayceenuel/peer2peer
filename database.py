fa8b81d2307a4f8fc054325f997b526c222a0805
Kaycee Nuel <heykayceee@gmail.com>
2025-09-04 15:59:19 +0100

    db connected

--- database.py
+++ database.py
@@ -5,3 +5,12 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "sqlite:///./wallet.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db(): 
+    db = SessionLocal()
+    try: 
+        yield db
+    finally: 
+        db.close()
