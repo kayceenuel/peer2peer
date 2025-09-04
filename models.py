c135259da2749d9cc7acf316fa81f23e1e99fa2e
Kaycee Nuel <heykayceee@gmail.com>
2025-09-04 12:17:32 +0100

    implemented transactions model

--- models.py
+++ models.py
@@ -13,3 +13,12 @@ class User(Base):
 
     transactions_sent = relationship("Transaction", foreign_keys="Transactions.sender_id")
     transactions_received = relationship("Transaction", foreign_keys="Transactions.receiver_id")
+
+class Transaction(Base): 
+    __tablename__ = "transactions" 
+
+    id = Column(Integer, primary_key=True, index=True) 
+    sender_id = Column(Integer, ForeignKey("users.id"))
+    receiver_id = Column(Integer, ForeignKey("users.id"))
+    amount = Column(Float, nullable=False)
+    timestamp = Column(DateTime(timezone=True), server_default=func.now())
