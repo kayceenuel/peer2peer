396b794132e0b6f3ab9c441a4774d151ef4984b3
Kaycee Nuel <heykayceee@gmail.com>
2025-09-04 12:00:08 +0100

    implemented Users data model

--- models.py
+++ models.py
@@ -1,8 +1,15 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, float 
-from sqlalchemy.orm import relationship 
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, func
+from sqlalchemy.orm import relationship
 from database import Base 
 
 class User(Base): 
     __tablename__ = "users" 
 
     id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    balance = Column(Float, defatult=0.0)
+
+    transactions_sent = relationship("Transaction", foreign_keys="Transactions.sender_id")
+    transactions_received = relationship("Transaction", foreign_keys="Transactions.receiver_id")
