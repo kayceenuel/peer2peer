25fca8ed9e0d1347876bc231ea2c8c4a0a88e950
Kaycee Nuel <heykayceee@gmail.com>
2025-09-05 19:39:15 +0100

    imported jwt statements

--- auth.py
+++ auth.py
@@ -1,4 +1,11 @@
 from passlib.context import CryptContext
+import os 
+from datetime import datetime, timedelta
+from typing import Union, Any
+from jose import jwt
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30 # Token expiration time in minutes 
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # Refresh token expiration time in mintes (7 days)
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -6,4 +13,4 @@ def get_hashed_password(password: str) -> str:
     return password_context.hash(password)
 
 def verify_password(password: str, hashed_password: str) -> bool: 
-    return password_context.verify(password, hashed_password)
+    return password_context.verify(password, hashed_password) 
