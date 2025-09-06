b3ed9d121b018f54bc7ba184d026035a4c9db9ee
Kaycee Nuel <heykayceee@gmail.com>
2025-09-06 15:42:15 +0100

    added login API Method

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -40,6 +40,7 @@ def signup(user: UserCreate, db: Session = Depends(get_db)):
 
     return {"msg": "User created sucesfully", "user_id": new_user.id} 
 
+@router.post("/login")
 def login(user: UserLogin, db: Session = Depends(get_db)):
     # Authenticate user and generate tokens 
     db_user = db.query(User).filter(User.email == user.email).first()
