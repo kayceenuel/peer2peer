ac9a01a126653d152ae53f9112be6f1909223fbd
Kaycee Nuel <heykayceee@gmail.com>
2025-09-06 12:14:02 +0100

    fix: misspeling

--- auth.py
+++ auth.py
@@ -1,12 +1,12 @@
 from passlib.context import CryptContext
-import os 
+import os # for env variables
 from datetime import datetime, timedelta
 from typing import Union, Any
 from jose import jwt
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30 # Token expiration time in minutes 
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # Refresh token expiration time in mintes (7 days)
-ALOGRITHM = "HS256"
+ALGORITHM = "HS256"
 JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY'] 
 JWT_REFRESH_SECRET_KEY = os.environ['JWT_REFRESH_SECRET_KEY']
 
@@ -26,7 +26,7 @@ def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> 
         expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
 
     to_encode = {"exp": expires_delta, "sub": str(subject)}
-    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALOGRITHM)
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
     return encoded_jwt
 
 def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str: 
@@ -36,5 +36,5 @@ def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) ->
         expires_delta = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
 
     to_encode = {"exp": expires_delta, "sub": str(subject)}
-    encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALOGRITHM)
+    encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
     return encoded_jwt
