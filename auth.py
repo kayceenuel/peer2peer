03508ca64bbebf1a0d2a9eb1b47bdd87ac11c4d7
Kaycee Nuel <heykayceee@gmail.com>
2025-09-06 11:59:46 +0100

    added access token function

--- auth.py
+++ auth.py
@@ -6,6 +6,10 @@ from jose import jwt
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30 # Token expiration time in minutes 
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # Refresh token expiration time in mintes (7 days)
+ALOGRITHM = "HS256"
+JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY'] 
+JWT_REFRESH_SECRET_KEY = os.environ['JWT_REFRESH_SECRET_KEY']
+
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -14,3 +18,13 @@ def get_hashed_password(password: str) -> str:
 
 def verify_password(password: str, hashed_password: str) -> bool: 
     return password_context.verify(password, hashed_password) 
+
+def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str: 
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else: 
+        expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALOGRITHM)
+    return encoded_jwt
