ffe45a5e353272e04e26184d1c969b3a9e8182ba
Kaycee Nuel <heykayceee@gmail.com>
2025-09-06 12:07:09 +0100

    added refresh token function

--- auth.py
+++ auth.py
@@ -28,3 +28,13 @@ def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> 
     to_encode = {"exp": expires_delta, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALOGRITHM)
     return encoded_jwt
+
+def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str: 
+    if expires_delta is not None: 
+        expires_delta = datetime.utcnow() + expires_delta
+    else: 
+        expires_delta = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALOGRITHM)
+    return encoded_jwt
