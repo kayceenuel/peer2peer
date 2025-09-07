80717d4e6edb9e4960b220b2fdca971c1969959b
Kaycee Nuel <heykayceee@gmail.com>
2025-09-07 16:13:40 +0100

    included routers

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
-from routes import auth_routes as auth_router
+from routes.auth_routes import router as auth_router
+from routes.wallet_routes import router as wallet_router
+from database import Base, engine
 
+# Create tables
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Routers
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(wallet_router, prefix="/wallet", tags=["wallet"])
